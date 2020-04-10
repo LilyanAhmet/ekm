d24a24f9d43e80c3808abaa8a0c5d553eab87b16
Ugur Köysüren <ugurkoysuren@gmail.com>
2020-04-10 15:24:53 +0300

    erfahrungen text added

--- src/pages/erfahrungen.js
+++ src/pages/erfahrungen.js
@@ -1,13 +1,30 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-const erfahrungen = () => {
-  return (
-    <Layout>
-      <SEO title="erfahrungen" />
-      <div>erfahrungen</div>
-    </Layout>
-  )
-}
+import { graphql } from "gatsby"
 
+class erfahrungen extends React.Component {
+    render() {
+        const graphData = this.props.data
+        console.log(graphData);
+        return (
+            <Layout>
+                <SEO title="erfahrungen"/>
+                <div>{graphData.allContentfulErfahrungen.nodes[0].text.text}</div>
+            </Layout>
+        )
+    }
+}
 export default erfahrungen
+
+export const query = graphql`
+    query  {
+  allContentfulErfahrungen {
+    nodes {
+      text {
+        text
+      }
+    }
+  }
+}
+`
