05298cf893b8544d3a4dd45af2d52d1c73cd5660
Ugur Köysüren <ugurkoysuren@gmail.com>
2020-04-13 17:46:48 +0300

    berater fixed

--- src/pages/berater.js
+++ src/pages/berater.js
@@ -19,6 +19,38 @@ export default class berater extends Component {
       })
     }
   }
+  createNavItems = (array) => {
+    let numbersth = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh', 'eighth'];
+    let navItems = []
+    let children = []
+    for (let i = 0; i < array.length; i++) {
+      children.push(<Nav.Link  eventKey={numbersth[i]}
+                                     className="col-12 col-xs-12 col-sm-12 col-md-12 col-lg">{array[i].node.name}</Nav.Link>)
+      navItems.push(<Nav.Item>{children[i]}</Nav.Item>)
+    }
+    return navItems
+    }
+  createTabPanes = (array) => {
+    let numbersth = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh', 'eighth'];
+    let navItems = []
+    for (let i = 0; i < array.length; i++) {
+      navItems.push(<Tab.Pane eventKey={numbersth[i]}>
+        <Row>
+          <Col md={3} sm={12}>
+            <img className="img-fluid" src={Avatar} />
+          </Col>
+          <Col md={9} sm={12}>
+            <p>
+              {array[i].node.description.description}
+            </p>
+          </Col>
+        </Row>
+      </Tab.Pane>)
+    }
+    return navItems
+  }
+
+
   render() {
     const graphData = this.props.data
     console.log(graphData);
@@ -38,68 +70,12 @@ export default class berater extends Component {
               <Row>
                 <Col sm={12}>
                   <Nav variant="pills" className="col-xs-12">
-                    <Nav.Item>
-                      <Nav.Link
-                        eventKey="first"
-                        className="col-12 col-xs-12 col-sm-12 col-md-12 col-lg"
-                      >
-                        Michael Schroeder
-                      </Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link
-                        eventKey="second"
-                        className="col-12 col-xs-12 col-sm-12 col-md-12 col-lg"
-                      >
-                        Dennis Klose
-                      </Nav.Link>
-                    </Nav.Item>
+                    {this.createNavItems(graphData.allContentfulConsultant.edges)}
                   </Nav>
                 </Col>
                 <Col sm={12}>
                   <Tab.Content>
-                    <Tab.Pane eventKey="first">
-                      <Row>
-                        <Col md={3} sm={12}>
-                          <img className="img-fluid" src={Avatar} />
-                        </Col>
-                        <Col md={9} sm={12}>
-                          <p>
-                            Michael Schroeder, Betriebswirt (VWA), seit über 30
-                            Jahren im Finanzdienstleistungssektor tätig, davon
-                            20 Jahre auf Executive Level bzw. in der
-                            Geschäftsführung. Schwerpunkttemen Kredit- und
-                            Aussenhandelsgeschäft, Finanzierung von
-                            Existenzgründern, Unternehmen, Projekten, privaten
-                            wie gewerblichen Bauvorhaben. Master Black-Belt im
-                            Rahmen der Prozeßoptimierung (SixSigma und Lean
-                            Management) und mehrjährige
-                            Projektmanagementerfahrung.
-                          </p>
-                        </Col>
-                      </Row>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="second">
-                      <Row>
-                        <Col md={3} sm={12}>
-                          <img className="img-fluid" src={Avatar} />
-                        </Col>
-                        <Col md={9} sm={12}>
-                          <p>
-                            Michael Schroeder, Betriebswirt (VWA), seit über 30
-                            Jahren im Finanzdienstleistungssektor tätig, davon
-                            20 Jahre auf Executive Level bzw. in der
-                            Geschäftsführung. Schwerpunkttemen Kredit- und
-                            Aussenhandelsgeschäft, Finanzierung von
-                            Existenzgründern, Unternehmen, Projekten, privaten
-                            wie gewerblichen Bauvorhaben. Master Black-Belt im
-                            Rahmen der Prozeßoptimierung (SixSigma und Lean
-                            Management) und mehrjährige
-                            Projektmanagementerfahrung.
-                          </p>
-                        </Col>
-                      </Row>
-                    </Tab.Pane>
+                    {this.createTabPanes(graphData.allContentfulConsultant.edges)}
                   </Tab.Content>
                 </Col>
               </Row>
@@ -112,19 +88,7 @@ export default class berater extends Component {
                   </div>
                   <div className="text">
                     <p>
-                      Wir begleiten Sie bereits im Rahmen der Existenzgründung
-                      und hier auch schon bei der Umsetzung Ihrer Idee in einen
-                      Businessplan und gern auch bei Finanzierungsfragen. bei
-                      der Entwicklung einer geeigneten Finanzierung, die aus
-                      Fördermöglichkeiten, privaten Investoren und
-                      Bankfinanzierungen besteht. Auch für bereits etablierte
-                      Unternehmen können wir gezielt durch eine SWAT-Analyse
-                      geplante Wachstumspläne überprüfen, Lösungen besprechen
-                      und Sie bis hin zur Findung einer geeigneten
-                      Finanzierungslösung begleiten. Wir unterstützen Sie bei
-                      den gewählten Schwerpunkten mit individuellen Lösungen und
-                      werden stets versuchen, vorhandene Fördermöglichkeiten
-                      aufzuzeigen und anzubieten.
+                      {graphData.allContentfulConsultant.edges[0].node.businessDevelopment.businessDevelopment}
                     </p>
                   </div>
                 </div>
@@ -138,20 +102,7 @@ export default class berater extends Component {
                   </div>
                   <div className="text">
                     <p>
-                      Haben Sie eine Idee, von der Sie glauben, dass diese Idee
-                      Sie ernähren könnte? Wir helfen Ihnen bei der Validierung
-                      Ihrer Idee und begleiten Sie auf dem Weg zum Businessplan
-                      und der Entscheidung, diese Idee weiter zu verfolgen mit
-                      dem Ziel, Sie bis zum Start Ihrer Tätigkeit zu
-                      unterstützen und Ihnen den Weg zu ebnen. Wir bieten Ihnen
-                      von der Förderung bei Existenzgründungen über private
-                      Investoren auch digitale Enabler an, die Ihnen mit Rat und
-                      Tat zur Seite stehen. Unsere Schwerpunkte liegen im
-                      Coaching des Gründers, der Entwicklung des Businessplans
-                      und der Usecases sowie der Erstellung einer SWAT-Analyse.
-                      Die Vollständigkeit der Finanzierungsunterlagen und ein
-                      gut aufbereiteter Finanzierungsvorschlag runden das
-                      gemeinsame Bankgespräch ab.
+                      {graphData.allContentfulConsultant.edges[0].node.existenzgrndungsberatung.existenzgrndungsberatung}
                     </p>
                   </div>
                 </div>
