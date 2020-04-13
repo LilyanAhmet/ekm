f4703191a771aede35b5037cc6cc45a96ee995f3
Ugur Köysüren <ugurkoysuren@gmail.com>
2020-04-13 09:33:52 +0300

    unternehmercoaching.js added.

---
+++ src/pages/unternehmercoaching.js
@@ -0,0 +1,52 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import { graphql } from "gatsby"
+
+class unternehmercoaching extends React.Component {
+    render() {
+        const graphData = this.props.data
+        console.log(graphData);
+        return (
+            <Layout>
+                <SEO title="unternehmercoaching"/>
+                <div>{graphData.allContentfulConsultant.edges[0].node.name}</div>
+                <div>{graphData.allContentfulConsultant.edges[0].node.description.description}</div>
+                <div>{graphData.allContentfulConsultant.edges[1].node.name}</div>
+                <div>{graphData.allContentfulConsultant.edges[1].node.existenzgrndungsberatung.existenzgrndungsberatung}</div>
+            </Layout>
+        )
+    }
+}
+export default unternehmercoaching
+
+export const query = graphql`
+       query {
+  allContentfulConsultant {
+    edges {
+      node {
+        name
+        photo {
+          fluid {
+            src
+          }
+        }
+        description {
+          description
+        }
+        businessDevelopment {
+          businessDevelopment
+        }
+        existenzgrndungsberatung {
+          existenzgrndungsberatung
+        }
+        zertifizierungen {
+          fluid {
+            src
+          }
+        }
+      }
+    }
+  }
+}
+`
