13d88d2c109eb725c7ff629fc16584bfb7c41b59
LilyanAhmetoglu <bayanxinon@gmail.com>
2020-04-14 17:23:08 +0300

    references implementing the  component

--- src/components/sharedComponents/referenceBox.js
+++ src/components/sharedComponents/referenceBox.js
@@ -1,17 +1,36 @@
 import React, { Component } from "react"
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import { graphql } from "gatsby"
-import { MDBContainer, MDBRow, MDBCol } from "mdbreact"
+
+/* resources */
+import Square from "../../images/small-square-green.svg"
+import Quote from '../../images/quote-green.svg'
+
 export default class referenceBox extends Component {
   render() {
     return (
-      <Layout>
-        <SEO title="unternehmercoaching" />
-        <MDBContainer size="lg">
-          <MDBRow></MDBRow>
-        </MDBContainer>
-      </Layout>
+      <div className="references">
+        <div className="box">
+          <div className="top">
+            <div className="left">
+                <img className="img-fluid" src={this.props.img} />
+                <div className="text">
+                    <h6>{this.props.name}</h6>
+                    <p>{this.props.title}</p>
+                </div>
+            </div>
+            <div className="right">
+                <img src={Quote}/>
+            </div>
+          </div>
+          <div className="bottom">
+            <p>
+              {this.props.description}
+            </p>
+          </div>
+          <div className="square">
+            <img src={Square} />
+          </div>
+        </div>
+      </div>
     )
   }
 }
--- src/pages/referenzen.js
+++ src/pages/referenzen.js
@@ -1,12 +1,49 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { graphql } from "gatsby"
+import { MDBContainer, MDBRow, MDBCol } from "mdbreact"
 
+/*components */
+import ReferenceBox from "../components/sharedComponents/referenceBox"
+import img from "../images/oval.png"
 const referenzen = () => {
   return (
     <Layout>
-      <SEO title="referenzen" />
-      <div>referenzen</div>
+      <SEO title="unternehmercoaching" />
+      <div className="references">
+        <MDBContainer size="lg">
+          <MDBRow>
+            <MDBCol md={12}>
+              <h1 className="borderimg">Du bist in einer guten Hand</h1>
+            </MDBCol>
+            <MDBCol md={12}>
+              <p>
+                Millionen von Menschen auf der ganzen Welt haben EKM bereits als
+                Ort gewählt, an dem ihr Traum wahr wird.
+              </p>
+            </MDBCol>
+          </MDBRow>
+          <MDBRow>
+            <MDBCol md={4} sm={12}>
+              <ReferenceBox
+                name="lilyan ahmetoglu"
+                title="consultant"
+                img={img}
+                description="There’s no way we could have hired these many people and gotten so
+                much business had we not had all of those back-office systems
+                figured out. It’s been easier growing our company with a system
+                that is so easy and scalable."
+              />
+            </MDBCol>
+            <MDBCol md={4} sm={12}></MDBCol>
+            <MDBCol md={4} sm={12}></MDBCol>
+            <MDBCol md={4} sm={12}></MDBCol>
+            <MDBCol md={4} sm={12}></MDBCol>
+            <MDBCol md={4} sm={12}></MDBCol>
+          </MDBRow>
+        </MDBContainer>
+      </div>
     </Layout>
   )
 }
