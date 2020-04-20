693132484f9b70887f9e7f29e4babfc2775b00ce
Ugur Köysüren <ugurkoysuren@gmail.com>
2020-04-20 09:53:46 +0300

    Blogs, slug added

--- src/components/blog/blogCard.js
+++ src/components/blog/blogCard.js
@@ -21,14 +21,14 @@ export default class blogCard extends Component {
                 src="https://mdbootstrap.com/img/Photos/Others/img%20(27).jpg"
                 alt=""
               />
-              <a href="#!">
+              <a href={this.props.slug}>
                 <MDBMask overlay="white-slight" />
               </a>
             </MDBView>
           </MDBCol>
           <MDBCol lg="7">
             <div className="text">
-              <a href="#!">
+              <a href={this.props.slug}>
                 <h6 className="font-weight-bold mb-3">{this.props.cat}</h6>
               </a>
               <h3 className="font-weight-bold mb-3 p-0">{this.props.title}</h3>
