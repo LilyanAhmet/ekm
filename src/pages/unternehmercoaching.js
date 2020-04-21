bf5003daa23d1fb9f28c0eb67773c1b1743ec77a
LilyanAhmetoglu <bayanxinon@gmail.com>
2020-04-21 11:23:18 +0300

    fixing references

--- src/pages/unternehmercoaching.js
+++ src/pages/unternehmercoaching.js
@@ -2,7 +2,7 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { graphql } from "gatsby"
-import {MDBContainer, MDBRow, MDBCol, MDBView, MDBMask} from "mdbreact"
+import { MDBContainer, MDBRow, MDBCol, MDBView, MDBMask } from "mdbreact"
 /*components */
 import Description from "../components/sharedComponents/description"
 import ServiceShort from "../components/sharedComponents/serviceShort"
@@ -11,46 +11,67 @@ import ServiceLong from "../components/sharedComponents/serviceLong"
 import DescImage from "../images/Unternehmercoaching.svg"
 
 class unternehmercoaching extends React.Component {
-
-    createServiceLong = (array) => {
-        let createServiceLong = []
-        for (let i = 0; i < array.length; i++) {
-            createServiceLong.push(
-                <ServiceLong
-                    img={array[i].image.fluid.src}
-                    icon={array[i].icon.fluid.src}
-                    title={array[i].title}
-                    shortDescription={array[i].description}
-                    contentOne={array[i].content1Title}
-                    contentOneText={array[i].content1Text}
-                    contentTwo={array[i].content2Title}
-                    contentTwoText={array[i].content2Text}
-                    contentThree={array[i].content3Title}
-                    contentThreeText={array[i].content3Text}
-                    left = {array[i].imageleft}         />
-            )
-        }
-        return createServiceLong
+  createServiceLong = array => {
+    let createServiceLong = []
+    for (let i = 0; i < array.length; i++) {
+      createServiceLong.push(
+        <ServiceLong
+          img={array[i].image.fluid.src}
+          icon={array[i].icon.fluid.src}
+          title={array[i].title}
+          shortDescription={array[i].description}
+          contentOne={array[i].content1Title}
+          contentOneText={array[i].content1Text}
+          contentTwo={array[i].content2Title}
+          contentTwoText={array[i].content2Text}
+          contentThree={array[i].content3Title}
+          contentThreeText={array[i].content3Text}
+          left={array[i].imageleft}
+        />
+      )
     }
-    createServiceShort = (array) => {
-        let createServiceShort = []
-        for (let i = 0; i < array.length; i++) {
-            createServiceShort.push(
-                <MDBCol md={6} sm={12}>
-                    <ServiceShort
-                        icon={array[i].icon.fluid.src}
-                        title={array[i].title}
-                        desciption={array[i].description}
-                    />
-                </MDBCol>
-            )
-        }
-        return createServiceShort
+    return createServiceLong
+  }
+  createServiceLongResponsive = array => {
+    let createServiceLong = []
+    for (let i = 0; i < array.length; i++) {
+      createServiceLong.push(
+        <ServiceLong
+          img={array[i].image.fluid.src}
+          icon={array[i].icon.fluid.src}
+          title={array[i].title}
+          shortDescription={array[i].description}
+          contentOne={array[i].content1Title}
+          contentOneText={array[i].content1Text}
+          contentTwo={array[i].content2Title}
+          contentTwoText={array[i].content2Text}
+          contentThree={array[i].content3Title}
+          contentThreeText={array[i].content3Text}
+          left={array[i].imageleft}
+        />
+      )
     }
+    return createServiceLong
+  }
+  createServiceShort = array => {
+    let createServiceShort = []
+    for (let i = 0; i < array.length; i++) {
+      createServiceShort.push(
+        <MDBCol md={6} sm={12}>
+          <ServiceShort
+            icon={array[i].icon.fluid.src}
+            title={array[i].title}
+            desciption={array[i].description}
+          />
+        </MDBCol>
+      )
+    }
+    return createServiceShort
+  }
 
   render() {
     const graphData = this.props.data
-    console.log(graphData.allContentfulServicePage);
+    console.log(graphData.allContentfulServicePage)
     return (
       <Layout>
         <SEO title="unternehmercoaching" />
@@ -64,11 +85,22 @@ class unternehmercoaching extends React.Component {
 
         <MDBContainer size="lg">
           <MDBRow>
-              {this.createServiceShort(graphData.allContentfulServicePage.edges[0].node.serviceShort)}
+            {this.createServiceShort(
+              graphData.allContentfulServicePage.edges[0].node.serviceShort
+            )}
           </MDBRow>
         </MDBContainer>
         <MDBContainer fluid className="bggray">
-            {this.createServiceLong(graphData.allContentfulServicePage.edges[0].node.serviceLong)}
+          <div className="desktop">
+            {this.createServiceLong(
+              graphData.allContentfulServicePage.edges[0].node.serviceLong
+            )}
+          </div>
+          <div className="mobile">
+            {this.createServiceLongResponsive(
+              graphData.allContentfulServicePage.edges[0].node.serviceLong
+            )}
+          </div>
         </MDBContainer>
       </Layout>
     )
@@ -77,45 +109,47 @@ class unternehmercoaching extends React.Component {
 export default unternehmercoaching
 
 export const query = graphql`
-{
-  allContentfulServicePage {
-    edges {
-      node {
-        title
-        text{text}
-        serviceShort {
+  {
+    allContentfulServicePage {
+      edges {
+        node {
           title
-          description
-          icon {
-            fluid {
-              src
-            }
+          text {
+            text
           }
-        }
-        serviceLong {
-          ... on ContentfulServiceLong {
+          serviceShort {
             title
-            imageleft
-            image {
-              fluid {
-                src
-              }
-            }
+            description
             icon {
               fluid {
                 src
               }
             }
-            content1Title
-            content1Text
-            content2Title
-            content2Text
-            content3Title
-            content3Text
+          }
+          serviceLong {
+            ... on ContentfulServiceLong {
+              title
+              imageleft
+              image {
+                fluid {
+                  src
+                }
+              }
+              icon {
+                fluid {
+                  src
+                }
+              }
+              content1Title
+              content1Text
+              content2Title
+              content2Text
+              content3Title
+              content3Text
+            }
           }
         }
       }
     }
   }
-}
 `
